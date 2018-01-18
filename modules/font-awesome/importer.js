636faaccbe1d32f2b53c0996a1e8dc530d9ce8a7
kaptinlin <kaptinlin@gmail.com>
2018-01-18 15:06:55 +0800

    update structure

--- modules/font-awesome/importer.js
+++ modules/font-awesome/importer.js
@@ -1,15 +1,16 @@
-import generateCss from './node_modules/@icon/support/utils/generateCss';
-import prepareIcons from './node_modules/@icon/support/utils/prepareIcons';
-import extraFromJson from './node_modules/@icon/support/utils/extraFromJson';
-import detectLicense from './node_modules/@icon/support/utils/detectLicense';
-import getIconsMap from './node_modules/@icon/support/utils/getIconsMap';
-import getIconsFromUrl from './node_modules/@icon/support/utils/getIconsFromUrl';
-import getIconsFromCss from './node_modules/@icon/support/utils/getIconsFromCss';
-import getFonts from './node_modules/@icon/support/utils/getFonts';
-import copyFonts from './node_modules/@icon/support/utils/copyFonts';
-import copyLicense from './node_modules/@icon/support/utils/copyLicense';
+import generateCss from '../../scripts/utils/generateCss';
+import prepareIcons from '../../scripts/utils/prepareIcons';
+import extraFromJson from '../../scripts/utils/extraFromJson';
+import detectLicense from '../../scripts/utils/detectLicense';
+import getIconsMap from '../../scripts/utils/getIconsMap';
+import getIconsFromUrl from '../../scripts/utils/getIconsFromUrl';
+import getIconsFromCss from '../../scripts/utils/getIconsFromCss';
+import getFonts from '../../scripts/utils/getFonts';
+import copyFonts from '../../scripts/utils/copyFonts';
+import copyLicense from '../../scripts/utils/copyLicense';
 import fs from 'fs-extra';
 import path from 'path';
+
 
 let options = {
   source: path.join('./node_modules/', 'font-awesome'),
