54a54a795a900f3fa9f42b9f37e741b3dd9792a9
Sepctre <sepctre@amazingsurge.com>
2018-01-22 09:54:30 +0800

    update feathericon

---
+++ modules/feathericon/importer.js
@@ -0,0 +1,52 @@
+let generateCss = require('../../scripts/utils/generateCss');
+let generateJson = require('../../scripts/utils/generateJson');
+let prepareIcons = require('../../scripts/utils/prepareIcons');
+let extraFromJson = require('../../scripts/utils/extraFromJson');
+let detectLicense = require('../../scripts/utils/detectLicense');
+let getIconsFromCss = require('../../scripts/utils/getIconsFromCss');
+let getSvgs = require('../../scripts/utils/getSvgs');
+let copySvgs = require('../../scripts/utils/copySvgs');
+let getFonts = require('../../scripts/utils/getFonts');
+let copyFonts = require('../../scripts/utils/copyFonts');
+let copyLicense = require('../../scripts/utils/copyLicense');
+let fs = require('fs-extra');
+let path = require('path');
+
+let options = {
+  source: path.join(`${__dirname}/node_modules/`, 'feathericon'),
+  name: 'feathericon',
+  class: 'fei',
+  prefix: 'fei-',
+  className: 'Feathericon',
+  title: 'Feathericon',
+  classifiable: false
+};
+
+let paths = {
+  package: path.join(options.source, 'package.json'),
+  css: path.join(options.source, 'build', 'css', 'feathericon.css'),
+  fonts: path.join(options.source, 'build', 'fonts'),
+  svgs: path.join(options.source, 'build', 'svg'),
+  dest: __dirname,
+  svgsDest: path.join(__dirname, 'icons')
+};
+
+let info = extraFromJson(paths.package, ['homepage', 'description', 'version', 'author', 'license']);
+
+options.license = info.license;
+options.author = info.author;
+options.homepage = info.homepage;
+options.description = info.description;
+options.version = info.version;
+options.fonts = getFonts(paths.fonts);
+options.svgs = getSvgs(paths.svgs, '*.svg');
+
+module.exports = function() {
+  options.icons = getIconsFromCss(paths.css, 'fe-');
+  options = prepareIcons(options);
+  generateCss(paths.dest, options.name, options);
+  generateJson(paths.dest, options.className, options);
+  copyFonts(paths.dest, paths.fonts, options.fonts);
+  copySvgs(paths.svgsDest, paths.svgs, options.svgs);
+  copyLicense(paths.dest, path.join(options.source, 'LICENSE'));
+};
