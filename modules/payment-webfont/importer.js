65ead5125a115e760fb9cd3ceeaaa7a7efed1de3
Sepctre <sepctre@amazingsurge.com>
2018-02-06 17:56:47 +0800

    add payment-webfont

---
+++ modules/payment-webfont/importer.js
@@ -0,0 +1,48 @@
+const generateCss = require('../../scripts/utils/generateCss');
+const generateJson = require('../../scripts/utils/generateJson');
+const prepareIcons = require('../../scripts/utils/prepareIcons');
+const extraFromJson = require('../../scripts/utils/extraFromJson');
+const detectLicense = require('../../scripts/utils/detectLicense');
+const getIconsFromCss = require('../../scripts/utils/getIconsFromCss');
+const getFonts = require('../../scripts/utils/getFonts');
+const copyFonts = require('../../scripts/utils/copyFonts');
+const generateSvgs = require('../../scripts/utils/generateSvgs');
+const copyLicense = require('../../scripts/utils/copyLicense');
+const fs = require('fs-extra');
+const config = require('../../config');
+const path = require('path');
+
+let options = {
+  source: path.join(config.sets.customs, 'payment-webfont'),
+  name: 'payment-webfont',
+  class: 'pw',
+  prefix: 'pw-',
+  className: 'Payment-webfont',
+  classifiable: false
+};
+
+let paths = {
+  package: path.join(options.source, 'bower.json'),
+  css: path.join(options.source, 'style.css'),
+  fonts: path.join(options.source, 'fonts'),
+  dest: __dirname
+};
+
+let info = extraFromJson(paths.package, ['homepage', 'description', 'version', 'author', 'license']);
+
+options.license = info.license;
+options.author = info.author;
+options.homepage = info.homepage;
+options.description = info.description;
+options.version = info.version;
+options.fonts = getFonts(paths.fonts);
+
+module.exports = function() {
+  options.icons = getIconsFromCss(paths.css, 'pw-');
+  options = prepareIcons(options);
+  generateCss(paths.dest, options.name, options);
+  generateJson(paths.dest, options.className, options);
+  copyFonts(paths.dest, paths.fonts, options);
+  copyLicense(paths.dest, path.join(options.source, 'LICENSE'));
+  generateSvgs(paths.dest, options.name, options);
+};
