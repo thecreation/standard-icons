375faa3ea394593fa12eef15c1c0ee1f183f3fbb
Sepctre <sepctre@amazingsurge.com>
2018-02-06 18:05:43 +0800

    add cryptocurrency-icons

---
+++ modules/cryptocurrency-icons/LICENSE
@@ -0,0 +1,36 @@
+# Cryptocurrency Icons
+
+This download contains over 2000 icons – 232 crypto, and fiat currencies, in 4 styles and a range of sizes. Completely free. All I ask is that you don’t claim them as your own, and share this resource with others.
+
+Images are in the PNG format (transparent) in 32×32, 64×64(@2x), 128×128, and 256×256(@2x) variants, along with SVG versions.
+
+If there is an altcoin you’re looking for that hasn’t been included, please create a new issue and I shall include it.
+
+
+![hero](img/hero@2x.jpg)
+
+
+## Donate
+
+If you find these icons useful, or if you’ve used them for your projects, please consider making a donation. I have invested over 110 hours in creating and maintaining this project, and your donations will help ensure it continues.
+
+**Bitcoin**
+`1JoyuwysWLtxd8twdXx6G1ZJ7DHf2jcu8Q`
+
+**Ethereum**
+`0x2a47661bc70478D965C0Fb6121309Cc877BD9508`
+
+**Litecoin**
+`LW2Nk8T24Dp69DoqaQX2srBGU6jYVx5A9X`
+
+
+## License
+
+>All icons are licensed under the **Creative Commons Zero (CC0) license**. This means the icons are completely free to be used for any legal purpose.
+>
+>- The icons are **free** for personal and even for commercial use.
+>- You can modify, copy and distribute the icons.
+>- All without asking for permission or setting a link to the source. So, **attribution is not required**.
+>
+>
+>The CC0 license was released by the non-profit organization Creative Commons (CC). Get more information about Creative Commons images and the license on the [official license page](https://creativecommons.org/publicdomain/zero/1.0/).
---
+++ modules/cryptocurrency-icons/importer.js
@@ -0,0 +1,49 @@
+const generateCss = require('../../scripts/utils/generateCss');
+const generateJson = require('../../scripts/utils/generateJson');
+const extraFromJson = require('../../scripts/utils/extraFromJson');
+const getIconsFromCss3 = require('../../scripts/utils/getIconsFromCss3');
+const prepareIcons = require('../../scripts/utils/prepareIcons');
+const getSvgs = require('../../scripts/utils/getSvgs');
+const copySvgs = require('../../scripts/utils/copySvgs');
+const copyLicense = require('../../scripts/utils/copyLicense');
+const generateFontsFromSvg = require('../../scripts/utils/generateFontsFromSvg');
+const fs = require('fs-extra');
+const path = require('path');
+
+let options = {
+  source: path.join(`${__dirname}/node_modules/`, 'cryptocurrency-icons'),
+  name: 'cryptocurrency-icons',
+  class: 'cryptocurrency-icons',
+  prefix: 'cryptocurrency-icons-',
+  className: 'Cryptocurrency Icons',
+  title: 'Cryptocurrency Icons',
+  classifiable: false
+};
+
+let paths = {
+  package: path.join(options.source, 'package.json'),
+  svgs: path.join(options.source, 'svg', 'black'),
+  dest: __dirname,
+  svgsDest: path.join(__dirname, 'icons')
+};
+
+let info = extraFromJson(paths.package, ['homepage', 'description', 'version', 'author', 'license']);
+
+options.license = info.license;
+options.author = info.author.name;
+options.homepage = info.homepage;
+options.description = info.description;
+options.version = info.version;
+options.svgs = getSvgs(paths.svgs);
+
+function callback() {
+  options.icons = getIconsFromCss3(`${__dirname}/${options.name}.css`, 'cryptocurrency-icons-');
+  options = prepareIcons(options);
+  generateJson(paths.dest, options.className, options);
+  copyLicense(paths.dest, path.join(options.source, 'README.md'));
+}
+
+module.exports = function() {
+  copySvgs(paths.svgsDest, paths.svgs, options.svgs);
+  generateFontsFromSvg(paths.dest, options, callback);
+};
