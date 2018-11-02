1c88df6be80ec76400490d541b980678172b654a
Sepctre <sepctre@amazingsurge.com>
2018-11-02 22:24:22 +0800

    add boxicons-regular solid logos

---
+++ modules/boxicons-regular/importer.js
@@ -0,0 +1,53 @@
+const generateCss = require('../../scripts/utils/generateCss');
+const generateJson = require('../../scripts/utils/generateJson');
+const prepareIcons = require('../../scripts/utils/prepareIcons');
+const extraFromJson = require('../../scripts/utils/extraFromJson');
+const detectLicense = require('../../scripts/utils/detectLicense');
+const getIconsFromCss = require('../../scripts/utils/getIconsFromCss');
+const getSvgs = require('../../scripts/utils/getSvgs');
+const copySvgs = require('../../scripts/utils/copySvgs');
+const getFonts = require('../../scripts/utils/getFonts');
+const copyFonts = require('../../scripts/utils/copyFonts');
+const copyLicense = require('../../scripts/utils/copyLicense');
+const jsonfile = require('../../scripts/utils/jsonfile');
+const fs = require('fs-extra');
+const path = require('path');
+
+let options = {
+  source: path.join(`${__dirname}/node_modules/`, 'boxicons'),
+  name: 'boxicons',
+  class: 'bx',
+  prefix: 'bxr-',
+  className: 'Boxicons',
+  title: 'Boxicons',
+  version: '1.7.1-1',
+  classifiable: false
+};
+
+let paths = {
+  package: path.join(options.source, 'package.json'),
+  css: path.join(options.source, 'css', 'boxicons.css'),
+  fonts: path.join(options.source, 'fonts'),
+  svgs: path.join(options.source, 'svg', 'regular'),
+  dest: __dirname,
+  svgsDest: path.join(__dirname, 'icons')
+};
+
+let info = extraFromJson(paths.package, ['homepage', 'description', 'author', 'license']);
+
+options.license = info.license;
+options.author = info.author.name;
+options.homepage = info.homepage;
+options.description = info.description;
+options.fonts = getFonts(paths.fonts);
+options.svgs = getSvgs(paths.svgs);
+
+module.exports = function() {
+  options.icons = getIconsFromCss(paths.css, 'bx-');
+  options = prepareIcons(options);
+  generateCss(paths.dest, options.name, options);
+  generateJson(paths.dest, options.className, options);
+  copyFonts(paths.dest, paths.fonts, options);
+  copySvgs(paths.svgsDest, paths.svgs, options.svgs, 'bx-');
+  jsonfile(paths.dest, options);
+};
