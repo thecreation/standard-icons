93939f86b5c53d735eda756ecfefaa624e9ffa7d
Sepctre <sepctre@amazingsurge.com>
2018-12-04 21:09:21 +0800

    update simple-icons

--- modules/simple-icons/importer.js
+++ modules/simple-icons/importer.js
@@ -9,6 +9,7 @@ const copyLicense = require('../../scripts/utils/copyLicense');
 const generateFontsFromSvg = require('../../scripts/utils/generateFontsFromSvg');
 const jsonfile = require('../../scripts/utils/jsonfile');
 const clean = require('../../scripts/utils/clean');
+const fs = require('fs-extra');
 const path = require('path');
 
 let options = {
@@ -40,6 +41,7 @@ options.svgs = getSvgs(paths.svgs);
 module.exports = function(callback) {
   clean(paths.dest);
   copySvgs(paths.svgsDest, paths.svgs, options.svgs);
+  fs.unlinkSync(`${paths.svgsDest}/viber.svg`);
   generateFontsFromSvg(paths.dest, options, () => {
     options.icons = getIconsFromCss3(`${__dirname}/${options.name}.css`, 'si-');
     options = prepareIcons(options);
